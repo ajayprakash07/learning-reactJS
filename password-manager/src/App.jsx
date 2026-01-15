8da9cbb1bb2481f0f8b162804779445ac613c218
Ajay Prakash <abhi9singh400@gmail.com>
2026-01-15 18:51:58 +0530

    added Password Strength indicator

--- password-manager/src/App.jsx
+++ password-manager/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   const [num, setNum] = useState(false)
   const [char, setChar] = useState(false)
   const [pass, setPass] = useState("")
+  const [strength, setStrength] = useState("WEAK")
+  const [col, setCol] = useState('white')
 
   //ref
   const passwordRef = useRef(null);
@@ -12,13 +14,31 @@ function App() {
   const gen = useCallback(() => {
     let p="";
     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let marks=0;
 
     if(num) str+= "0123456789"
     if(char) str+= "!@#$%^&*()[]{}<>"
+    if(length>8) marks+=3
 
     for(let i=0;i<length;i++){
       let ch = Math.floor(Math.random()* str.length +1)
       p += str.charAt(ch)
+    }
+
+    if(/[a-z]/.test(p)) marks+=2
+    if(/[A-Z]/.test(p)) marks+=2
+    if(/[0-9]/.test(p)) marks+=2
+    if(/[^A-Za-z0-9]/.test(p)) marks+=2
+
+    if(marks<5) {
+      setStrength("WEAK")
+      setCol('red')
+    }else if(marks>4 && marks<8) {
+      setStrength("MEDIUM")
+      setCol('yellow')
+    }else {
+      setStrength("STRONG")
+      setCol('green')
     }
 
     setPass(p)
@@ -47,7 +67,7 @@ function App() {
             readOnly
             ref={passwordRef}
            />
-           <button className='bg-yellow-300 px-2 rounded-lg px-1 py-1 hover:bg-yellow-200 border-black border'
+           <button className='text-black font-xl bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 rounded-lg'
            onClick={copyPassword}
            >copy</button>
         </div>
@@ -84,6 +104,11 @@ function App() {
             <label>Characters</label>
           </div>
         </div>
+      </div>
+
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-2 text-black bg-white"
+      style={{backgroundColor: col}}>
+        <p className='text-center font-semibold'>STRENTH: {strength}</p>
       </div>
     </>
   )
