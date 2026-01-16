eef1fc570459fe923ac907d016cfa1be81ac26e8
Ajay Prakash <abhi9singh400@gmail.com>
2026-01-16 20:26:36 +0530

    Basic routing done

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div className='bg-red-300'>hello</div>
     </>
   )
 }
---
+++ router/src/Layout.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header/Header.jsx'
+import Footer  from './components/Footer/Footer.jsx'
+import Home from './components/Home/Home.jsx'
+import { Outlet } from 'react-router-dom'
+import React from 'react'
+
+function Layout(){
+    return(
+        <>
+        <Header />
+        <Outlet />
+        <Footer />
+        </>
+    )
+}
+
+export default Layout
