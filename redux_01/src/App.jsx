9c10c182a9b31f133aac679d6aba4ead1a6ae034
Ajay Prakash <abhi9singh400@gmail.com>
2026-01-26 19:15:19 +0530

    started redux

---
+++ redux_01/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Testing...</div>
+    </>
+  )
+}
+
+export default App
