c71402813770456adddcb05568d54e50b8b09592
Ajay Prakash <abhi9singh400@gmail.com>
2026-06-12 13:43:07 +0530

    started blog project and staing prev work

---
+++ blog-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+   <>
+   <div>Working...</div>
+   </>
+  )
+}
+
+export default App
