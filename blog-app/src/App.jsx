5d22e5fec32d8fd3faf18a8f123732ffe53ce43b
Ajay Prakash <abhi9singh400@gmail.com>
2026-06-15 16:28:10 +0530

    redux store was config

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import  {login , logout} from './store/authSlice'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-   <>
-   <div>Working...</div>
-   </>
-  )
+  
+  const [loading , setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false));
+  }, [])
+
+  return !loading ? (
+    <>
+    
+    <Header/>
+    <div className='min-h-screen bg-red-500 flex flex-wrap'>hello</div>
+    <Footer/>
+    </>
+  ) : null
 }
 
 export default App
